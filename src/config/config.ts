a086006c6c21782be0e4769b5c76eb565c0d105b
SayantikaDey2004 <sayantika.dey2004@gmail.com>
2026-02-21 03:05:02 +0530

    feat: update API base URL to production environment

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,4 @@
-export const url = "http://localhost:5000";
+// export const url = "http://localhost:5000";
+export const url = "https://college-admin-backend-1orc.onrender.com";
 export const version = "v1";
 export const BASE_URL: string = `${url}/api/${version}`;
